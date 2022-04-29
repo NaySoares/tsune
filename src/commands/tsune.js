777bae13c24b485483d16e5a7bd040ea66c61616
Elienai <elienay.soares07@gmail.com>
2022-04-28 23:43:25 -0300

    add variant for command ?tsune

--- src/commands/tsune.js
+++ src/commands/tsune.js
@@ -1,7 +1,15 @@
+const users = require('../configs/users')
+const {MessageAttachment} = require('discord.js');
+
 module.exports= {
   name: 'tsune',
   description: 'Uma msg de olá',
-  execute(msg, args){
-    msg.channel.send('Eu sou a Tsune, vou ajudar como puder')
+  execute(bot, msg, args){
+    if(msg.author.id === users.axios){
+      const attachments = new MessageAttachment('src/assets/imgs/tsune.jpg')
+      msg.channel.send('A seu dispor, my master.', {files: [attachments]})
+    } else {
+      msg.channel.send('Saiba que sou muita areia pro seu caminhãzinho.')
+    }
   }
 }
