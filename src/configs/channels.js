54fdf62293f52cdbe837e61141e7fe23d6709a10
Elienai <elienay.soares07@gmail.com>
2022-05-25 21:23:08 -0300

    set channel hinowa

--- src/configs/channels.js
+++ src/configs/channels.js
@@ -3,7 +3,7 @@ const channels = {
   avisos: '970503339601178647',
   //--tsundoku--//
   menu: '765212713705930852',
-  hinowaGaCrush: '969303901847302214',
+  hinowaGaCrush: '795727440835510304',
 }
 
 module.exports = channels;
