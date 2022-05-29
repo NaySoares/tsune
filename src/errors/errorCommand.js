a68e6983e5414a91504998ee3a9784a7d50907d7
Elienai <elienay.soares07@gmail.com>
2022-05-29 06:09:26 -0300

    create message error

---
+++ src/errors/errorCommand.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+const createBot = require('../configs/bot/botDiscord');
+const channels = require('../configs/channels');
+
+module.exports = ErrorCommand = {
+  async execute(bot, msg, command) {
+    const channelTarget = await bot.channels.fetch(channels.logs)
+    const msgEmbed = new Discord.MessageEmbed()
+      .setColor('#ff0000')
+      .setTitle(`Erro no comando ${command}`)
+      .setDescription(`${msg}`)
+    channelTarget.send(msgEmbed)
+  }
+}
