8ed21cd8edd32b519d12ad4282b0876350b3d996
Elienai <elienay.soares07@gmail.com>
2023-08-05 18:52:07 -0300

    upgrade in warn command

--- src/commands/warn.js
+++ src/commands/warn.js
@@ -3,11 +3,12 @@ const errorCommand = require('../errors/errorCommand');
 
 module.exports = {
   name: 'warn',
-  description: 'Send message for a channel',
+  description: 'Send message for a especific channel',
   async execute(bot, msg, args) {
     let channelTarget;
     const userAuthorized = Object.values(groups.owner).includes(msg.author.id);
     const contentMsg = args.slice(1).join(' ');
+    const imgAttachment = msg.attachments.first();
 
     try {
       channelTarget = await bot.channels.fetch(args[0]);
@@ -17,14 +18,33 @@ module.exports = {
 
     try {
       if (userAuthorized) {
-        if (!contentMsg) {
+        if (!contentMsg && !imgAttachment) {
           return msg.reply(
             'Não posso enviar uma mensagem vazia! Informe a mensagem após o ID do canal',
           );
         }
-        channelTarget.send({
-          content: contentMsg,
-        });
+
+        if (imgAttachment && contentMsg) {
+          channelTarget.send({
+            content: contentMsg,
+            files: [imgAttachment.url],
+          });
+          return;
+        }
+
+        if (imgAttachment && !contentMsg) {
+          channelTarget.send({
+            files: [imgAttachment.url],
+          });
+          return;
+        }
+
+        if (!imgAttachment && contentMsg) {
+          channelTarget.send({
+            content: contentMsg,
+          });
+          return;
+        }
       } else {
         return msg.reply('Comando restrito!');
       }
